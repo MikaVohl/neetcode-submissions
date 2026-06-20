b6f584de223ac7d41f78694ed5ccff5c1cc47880
Mika Vohl <103958325+MikaVohl@users.noreply.github.com>
2026-06-20 08:34:58 -0600

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        return "".join(chr(len(s)) + s for s in strs)
+
+    def decode(self, s: str) -> List[str]:
+        out = []
+        i, n = 0, len(s)
+        while i < n:
+            length = ord(s[i])
+            i += 1
+            out.append(s[i:i + length])
+            i += length
+        return out
+
+                
